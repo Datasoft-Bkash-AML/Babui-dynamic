0c0426c4a0eb87f6b4bfc27b6c5fae22703ccb27
Datasoft-Bkash-AML <ashif.islam006@gmail.com>
2025-08-24 05:33:57 +0000

    feat: update Shop controller for dynamic product display

--- application/controllers/Shop.php
+++ application/controllers/Shop.php
@@ -1,9 +1,27 @@
 <?php
 
-class Shop extends CI_Controller {
 
+require_once __DIR__ . '/../models/Product_model.php';
+
+class Shop extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('client-view/shop');
+		$productModel = new Product_model();
+		$products = $productModel->getAll();
+		$this->load->view('client-view/shop', ['products' => $products]);
+	}
+
+	public function category($category)
+	{
+		$productModel = new Product_model();
+		$products = $productModel->getAll(['category' => $category]);
+		$this->load->view('client-view/shop', ['products' => $products, 'category' => $category]);
+	}
+
+	public function featured()
+	{
+		$productModel = new Product_model();
+		$products = $productModel->getAll(['is_featured' => true]);
+		$this->load->view('client-view/shop', ['products' => $products, 'featured' => true]);
 	}
 }
